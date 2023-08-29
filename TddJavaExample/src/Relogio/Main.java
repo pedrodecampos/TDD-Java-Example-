f1ae29f7e006e5d959f848955e08745cf28c9539
Pedro <pedroafonso939@Yahoo.com.br>
2023-08-29 14:55:59 -0300

    Segunda Parte Tdd - Implementando

---
+++ TddJavaExample/src/Relogio/Main.java
@@ -0,0 +1,59 @@
+package Relogio;
+
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+
+
+      // inicializando a classe relogio
+      Relogio relogio = new Relogio();
+
+      // criando um input ( scanner)
+        Scanner input = new Scanner(System.in);
+
+        int aux = 9999;
+        int hora, minuto,segundo=0;
+        while (aux != 0 ){
+            System.out.println("1 Registrar um horario");
+            System.out.println("2 iniciar o cronometro");
+            System.out.println("3 reniciar o relogio para meia-noite");
+            System.out.println("4 imprimir horario padrao 00:00:00");
+            System.out.println("5 imprimir horario padrao AM:PM");
+
+
+            System.out.println("0 sair ");
+
+            System.out.println("Digite a opocao que deseja: ");
+            aux = input.nextInt();
+
+            // criando o menu
+           switch (aux){
+               case 1:
+                   System.out.println("Digite a Hora: ");
+                   hora = input.nextInt();
+                   System.out.println("Digite o Minuto: ");
+                   minuto = input.nextInt();
+                   System.out.println("Digite o segundo: ");
+                   segundo = input.nextInt();
+                   relogio.horario(hora,minuto,segundo);
+                   break;
+               case 2:
+                   relogio.cronometro();
+                   break;
+               case 3:
+                   relogio.zerarRelogio();
+                   break;
+               case 4 :
+                   relogio.imprimirHorarioPadrao();
+                   break;
+               case 5:
+                   relogio.imprimirAmPm();
+                   break;
+
+               default: System.out.println("Digite uma opcao valida");
+               break;
+           }
+        }
+
+    }
+}
---
+++ TddJavaExample/src/Relogio/Relogio.java
@@ -0,0 +1,89 @@
+package Relogio;
+import java.util.Scanner;
+public class Relogio {
+
+    private int hora, minuto, segundo = 0;
+
+    // criando o construtor cheio/vazio
+  public void  horario(int hora, int minuto, int segundo){
+        this.hora = hora;
+        this.minuto = minuto;
+        this.segundo = segundo;
+    }
+
+    Relogio(){
+
+    }
+
+    // metodo para imprimir horario padrao 00:00:00
+    void imprimirHorarioPadrao(){
+        System.out.println("Horario Digitado: "+ "" + hora + ":"+  "" + minuto + ":"+  "" + segundo);
+    }
+
+    // metodo para imprimir no padrao AM/PM
+    void imprimirAmPm(){
+    	int Am, Pm;
+    	
+        if ( hora <= 12 )
+        	System.out.println("Hora Digitada AM:" + hora + "" + minuto);
+        else 
+        	System.out.println(" Hora Digitada PM:" + hora +  "" + minuto );
+    }
+
+    // metodo para zerar o relogio
+    void zerarRelogio(){
+        hora = 0;
+        minuto = 0;
+        segundo = 0;
+        System.out.print("Horario Reniciado Para 00h00");
+    }
+
+    // metodo para iniciar cronometro
+    void cronometro(){
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Clique no botao enter para iniciar: ");
+        input.nextLine();
+
+       // variavel que contem o metodo currentTime para salvar o tempo
+        long inicio = System.currentTimeMillis();
+
+        System.out.println("Clique no botao enter para parar : ");
+        input.nextLine();
+
+        // variavel para pegar o tempo novamente
+        long fim = System.currentTimeMillis();
+
+        // calcular a diferenca de fim - inicio
+        long calc = fim - inicio;
+        long prox=0;
+        // caluclando segundos
+        prox = calc/1000;
+        System.out.println("Tempo On: " + prox + " Segundos");
+    }
+
+    public int getHora() {
+        return hora;
+    }
+
+    public void setHora(int hora) {
+        this.hora = hora;
+    }
+
+    public int getMinuto() {
+        return minuto;
+    }
+
+    public void setMinuto(int minuto) {
+        this.minuto = minuto;
+    }
+
+    public int getSegundo() {
+        return segundo;
+    }
+
+    public void setSegundo(int segundo) {
+        this.segundo = segundo;
+    }
+
+}
